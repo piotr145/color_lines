c93bb617ab689a9ce26f2ede2d6e4ff5c510bfae
Piotr Bujakowski <piotrb2727@gmail.com>
2015-11-16 01:19:23 +0100

    move functions to private section

--- src/board.h
+++ src/board.h
@@ -4,21 +4,23 @@
 
 class Board
 {
-protected:
+public:
     int tab[9][9];
     int to_remove[9][9];
     int free_ctr;
     int colors;
     int points = 0;
-public:
-    Board(int colors);
-    bool make_move(sf::Vector2u start, sf::Vector2u stop);
-    bool check_for_move(sf::Vector2u start, sf::Vector2u stop);
-    void mark_to_rem();
+
     std::vector<std::vector<sf::Vector2u>> get_lines();
     int count_marked();
     void remove_marked();
     void place_new(int i);
+    bool check_for_move(sf::Vector2u start, sf::Vector2u stop);
+
+public:
+    Board(int colors);
+    bool make_move(sf::Vector2u start, sf::Vector2u stop);
+    void mark_to_rem();
     bool is_playing();
     int get_points();
     void draw();
